7850a415592f0239f718c26e9e6b7fee5e9f0586
Fernando Parada <fernandoparada18@protonmail.com>
2026-09-15 09:32:54 -0400

    Updates

--- assets/FullScreenLayout-Bvtv9t6N.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as s,b as t,e as r,o as c}from"./index-DI6ET3qw.js";const f="/project_yf/images/logo/auth-logo.svg",a={},n={class:"min-h-screen"};function _(e,l){return c(),s("div",n,[t("main",null,[r(e.$slots,"default")])])}const d=o(a,[["render",_]]);export{d as F,f as _};
